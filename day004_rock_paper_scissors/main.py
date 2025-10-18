03603d45de8077761ac980b235375b936805d3dc
rowbypierre <rowbypierre1998@gmail.com>
2025-10-17 21:16:47 -0400

    Clean, format, reduce: Ruff check and lint Functions to clear terminal, uniform printing + terminal delays Error handling

--- day004_rock_paper_scissors/main.py
+++ day004_rock_paper_scissors/main.py
@@ -1,83 +1,109 @@
-import time 
-import os 
+import time
+import os
 import random
-import sys
 
-rock = '''
+
+def clear_screen():
+    """Clear terminal screen."""
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def pause(seconds):
+    """Create delay in terminal screen for provided number of seconds (float)."""
+    time.sleep(seconds)
+
+
+def clear_print(string, delay):
+    """Clear terminal screen and print strings(s), followed by delay (int - secs)"""
+    obj_types = [list, str]
+    obj_type = type(string)
+    if obj_type in obj_types:
+        clear_screen()
+
+        if isinstance(string, list):
+            for line in string:
+                print(line)
+        else:
+            print(string)
+        pause(delay)
+
+
+rock = """
     _______
 ---'   ____)
       (_____)
       (_____)
       (____)
 ---.__(___)
-'''
+"""
 
-paper = '''
+paper = """
     _______
 ---'   ____)____
           ______)
           _______)
          _______)
 ---.__________)
-'''
+"""
 
-scissors = '''
+scissors = """
     _______
 ---'   ____)____
           ______)
        __________)
       (____)
 ---.__(___)
-'''
+"""
 
-while 1 == 1:
-    os.system("cls" if os.name == 'nt' else "clear")
-    print("Rock, paper, scissors VS the Computer.")
-    time.sleep(2)
-    os.system("cls" if os.name == 'nt' else "clear")
+options = {1: rock, 2: paper, 3: scissors}
+options_list = [opt for opt in options.keys()]
 
-    print("Enter 1 for rock. \nEnter 2 for paper. \nEnter 3 for scissors. \nEnter 'exit' to exit.")
-    time.sleep(2)
-    user_choice = input("\nEnter choice: ")
-    time.sleep(2)
-    if user_choice.isdigit() == True:
-        user_choice = int(user_choice)
-        if user_choice > 0 and user_choice < 4:
-            if user_choice == 1:
-                user_choice = rock
-            elif user_choice == 2:
-                user_choice = paper
-            elif user_choice == 3:
-                user_choice = scissors
-                
-            choices = [rock, paper, scissors]
-            computer_choice = choices[random.randint(0,2)] 
-            print(f"\nYour choice: {user_choice} \nComputer's choice: {computer_choice}")
-            if computer_choice == rock and user_choice == scissors:
-                print("\nXXXTHE COMPUTER WINSXXX")
-                time.sleep(5)  
-            elif computer_choice == scissors and user_choice == paper:
-                print("\nXXXTHE COMPUTER WINSXXX")
-                time.sleep(5)  
-            elif computer_choice == paper and user_choice == rock:
-                print("\nXXXTHE COMPUTER WINSXXX")
-                time.sleep(5)
-            elif computer_choice == user_choice:
-                print("\nWE HAVE A TIE. Try again.")
-                time.sleep(5)
-            else:
-                print("\n!!!YOU WIN!!!")
-                time.sleep(5)
-        
-        else:
-            print("\nValue must be 1, 2, or 3.")
-            time.sleep(2)
-            os.system("cls" if os.name == "nt" else "clear")
-            
-    elif user_choice.lower() == 'exit':
-        time.sleep(2)
-        sys.exit() 
-             
-    else:
-        print("\nEnter numeric value 1, 2, or 3.")
-        time.sleep(2)
+prompts = {
+    "welcome": "Rock, paper, scissors VS the Computer.",
+    "choices": ["Enter 1 for rock.", "\nEnter 2 for paper.", "\nEnter 3 for scissors."],
+}
+
+not_raise_ve = True
+game_on = True
+while game_on:
+    clear_print(prompts["welcome"], delay=2)
+    clear_print(prompts["choices"], delay=2)
+
+    try:
+        user_choice = input("\nEnter choice: ").strip()
+        not_raise_ve = user_choice.isdigit()
+
+        if not_raise_ve:
+            not_raise_ve = int(user_choice) in options_list
+            if not_raise_ve:
+                user_choice = options[int(user_choice)]
+                computer_choice = options[random.randint(1, 3)]
+
+                print(
+                    f"\nYour choice: {user_choice} \nComputer's choice: {computer_choice}"
+                )
+
+                if (
+                    computer_choice == rock
+                    and user_choice == scissors
+                    or computer_choice == scissors
+                    and user_choice == paper
+                    or computer_choice == paper
+                    and user_choice == rock
+                ):
+                    print("\nXXXTHE COMPUTER WINSXXX")
+                    game_on = False
+                elif computer_choice == user_choice:
+                    print("\nWE HAVE A TIE. Try again.")
+                else:
+                    print("\n!!!YOU WIN!!!")
+                    game_on = False
+
+        if not not_raise_ve:
+            game_on = False
+            raise ValueError(
+                f"Invalid input: {user_choice}\nValid inputs include: {options_list}"
+            )
+
+    except Exception as error:
+        print("Error: " + str(error))
