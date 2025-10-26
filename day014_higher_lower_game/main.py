2cb45921909cf7d1dcb5f8cf523060ee55569053
rowbypierre <rowbypierre1998@gmail.com>
2025-10-26 18:22:53 -0400

    Clean, simplify, improve code Add docstrings. Functionize repeating code. Increase readibility. Ruff check and formatter.

--- day014_higher_lower_game/main.py
+++ day014_higher_lower_game/main.py
@@ -1,65 +1,78 @@
-import art, random, os, time, game_data
-def higher_lower_game():
+import art
+import random
+import os
+import time
+import game_data
+
+
+def clear_screen():
+    """Clears termainl screen of Windows or UNIX-like operating systems."""
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def two_random_celebs():
+    """Return (list) two random celebs (dict) from game_data.py"""
+    return random.sample(game_data.data, 2)
+
+
+def guess_correct(celeb_a, celeb_b, guess):
+    """Confirms (bool) game players guess."""
+    if celeb_a["follower_count"] > celeb_b["follower_count"]:
+        popular_celeb = "A"
+    else:
+        popular_celeb = "B"
+
+    return guess == popular_celeb
+
+
+def game_display(celeb_list, score):
+    """Terminal game layout display."""
+    clear_screen()
+    print(
+        art.logo
+        + f"\nSCORE: {score}"
+        + f"\nCompare A: {celeb_list[0]['name']}, a {celeb_list[0]['description']}, from {celeb_list[0]['country']}."
+        + art.vs
+        + f"\nCompare B: {celeb_list[1]['name']}, a {celeb_list[1]['description']}, from {celeb_list[1]['country']}."
+    )
+
+
+def higher_lower():
+    """Guess celebrity with greater social media following:
+
+    Prompt:
+        - guess (str): A for celeb, A B for celeb B.
+
+    Return:
+        None: Print screen and ascii art confirming correct guess.
+
+    Raises:
+        ValueError: guess (str) prompt not 'A' or 'B'.
     """
-    Determine social media account with greater number of followers. Gameplayers input (numeric value '1' or '2') required
-    to play this game. If the user successfully completes 49 comparison, they are prompted to play the game again.
-    """
-    print(art.logo + "\nGuess the person or organization with MORE social media followers?")
+    score = 0
+    valid_inputs = ["A", "B"]
+    at_play = True
+    while at_play:
+        celebs = two_random_celebs()
+        print(celebs)
+        game_display(celebs, score)
 
-    data = game_data.data
-    random.shuffle(data)
-    dataset_size = len(data)
-    continue_play = True
+        try:
+            guess = input("Who has more followers? Type 'A' or 'B': ").strip().upper()
+            if guess not in valid_inputs:
+                raise ValueError(f"Guess {guess} is invalid. Expected: {valid_inputs}.")
+        except ValueError as ve:
+            print(ve)
 
-    while continue_play is True:
-        def clear_screen():
-            """
-            Clear terminal screen (windows or linux OS).
-            """
-            time.sleep(2)
-            os.system("cls" if os.name == "nt" else "clear")
-        
-        clear_screen()
-        account1 = data[random.randint(0, dataset_size - 1)]
-        iteration = 0
+        if guess_correct(*celebs, guess):
+            score += 1
+            print(art.right)
+        else:
+            print(art.wrong)
+            at_play = not at_play
 
-        for x in range(dataset_size):
-            game_streak = iteration  
-            iteration += 1
-            
-            account2 = data[x]
-            if account2["name"] == account1["name"]:
-                pass
-            else:
-                print(f"# CORRECT:\t{game_streak}")
-                print(f"\nAccount 1: \nName\t\t- {account1['name']}, \nDescription\t- {account1['description']}, \nCountry\t\t- {account1['country']}")
-                print(art.vs)
-                print(f"Account 2: \nName\t\t- {account2['name']}, \nDescription\t- {account2['description']}, \nCountry\t\t- {account2['country']}")
-                user_guess = int(input(f"\nWho has more followers? \nEnter 1 --> {account1['name']} \nEnter 2 --> {account2['name']} \n> "))
+        time.sleep(1)
 
-                if account1['follower_count'] > account2['follower_count']:
-                    correct_answer = 1
-                else: 
-                    correct_answer = 2
-                
-                if user_guess == correct_answer:
-                    print(art.right + f"{account1['name']} has {account1['follower_count']} million followers.")
-                    print(f"{account2['name']} has {account2['follower_count']} million followers.")
-                    if correct_answer == 2:
-                        account1 = account2
-                    clear_screen()    
-                else:
-                    print(art.wrong)
-                    continue_play = False
-                    clear_screen()
-                    break
-            
-            if iteration == dataset_size and input("\nWould you like to play again? \nEnter 'y'\t--> Yes. \nEnter 'n'\t--> No. \n> ").strip().lower() == 'y':
-                continue_play = True
-            else:
-                continue_play = False    
-                
+
 if __name__ == "__main__":
-    higher_lower_game()
-    
-    
+    higher_lower()
