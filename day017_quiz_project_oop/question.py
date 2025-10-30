c00434c8c92e7238f35c3ad8f7b69d8135450dbf
rowbypierre <rowbypierre1998@gmail.com>
2025-10-29 21:26:17 -0400

    Add docstrings

--- day017_quiz_project_oop/question.py
+++ day017_quiz_project_oop/question.py
@@ -1,4 +1,5 @@
 class Question:
     def __init__(self, question, answer):
+        """Initialize question with correct anwser."""
         self.question = question
         self.answer = answer
--- day017_quiz_project_oop/quiz.py
+++ day017_quiz_project_oop/quiz.py
@@ -3,15 +3,26 @@ import random
 
 class Quiz:
     def __init__(self, question_list):
+        """Initialize quiz with question bank."""
         self.question_number = 0
         self.question_list = question_list
         self.correct_answers = 0
         self.quiz_length = len(self.question_list)
 
     def questions_remain(self):
+        """Confirm (bool) if unprompted questions remain."""
         return self.question_number < self.quiz_length
 
     def next_question(self):
+        """
+        Prompt quiz taker next question.
+
+        Prompt:
+            - user_answer (str)
+
+        Return: 
+            - None
+        """
         random.shuffle(self.question_list)
         current_question = self.question_list[self.question_number]
         self.question_number += 1
@@ -21,6 +32,7 @@ class Quiz:
         self.answer_check(user_answer, current_question.answer)
 
     def answer_check(self, user_answer, correct_answer):
+        """Compare two answers (str) for match. """
         evaluation = user_answer.lower() == correct_answer.lower()
         if evaluation:
             self.correct_answers += 1
@@ -35,7 +47,8 @@ class Quiz:
         )
 
     def is_quiz_complete(self):
-        if self.question_number == len(self.question_list):
+        """Print final score if quiz is completed (no questions remain)."""
+        if not self.questions_remain():
             print(
                 "Quiz completed."
                 f"\nFinal score:\t{self.correct_answers}\\{self.quiz_length}"
