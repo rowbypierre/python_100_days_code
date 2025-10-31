d5aaa92de2d2ad3e5e1029419aa59513c4831060
rowbypierre <rowbypierre1998@gmail.com>
2025-10-31 17:30:53 -0400

    Clean and reformat code

--- day018_bot_dot_art_oop/main.py
+++ day018_bot_dot_art_oop/main.py
@@ -1,65 +1,45 @@
-import turtle
-import turtle as tt
-import colorgram as cg
-import random as rm
+import turtle as T
+import colorgram as CG
+import random as RM
 
-colours = cg.extract("./images/damien_hirst_print.jpg", 21)
-# print(colours)
-
+colours = CG.extract("./images/damien_hirst_print.jpg", 21)
 background = colours[0]
-# print(background)
-rgb_colors = []
-
-for color in colours:
-    if color != background:
-        r_hue = color.rgb[0]
-        g_hue = color.rgb[1]
-        b_hue = color.rgb[2]
-        color_rgb = (r_hue, g_hue, b_hue)
-        rgb_colors.append(color_rgb)
-# print(rgb_colors)
+rgb_colors = [
+    (color.rgb[0], color.rgb[1], color.rgb[2])
+    for color in colours
+    if color != background
+]
 
 # Todo: 1. Create custom window.
-turtle.screensize(250, 250)
-print(tt.screensize())
+screen = T.Screen()
+screen.setup(width=700, height=700)
+screen.screensize(250, 250)
+screen.colormode(255)
 
 # Todo: 2. Create turtle.
-turtle = tt.Turtle()
+turtle = T.Turtle(shape="turtle")
 turtle.color("black")
-turtle.shape("turtle")
 turtle.speed("fastest")
 
 
-def turtle_info():
-    """
-    Returns turtles coordinated position and angle (0 = east).
-    """
-    print(f"Position:\t{turtle.pos()}"
-          f"\nAngle:\t\t{turtle.heading()}")
-
-
 # Todo: 3. Teleport turtle to start position.
-start_x = -200
-start_y = -200
-turtle.teleport(start_x, start_y)
+start_xy = (-200, -200)
+turtle.teleport(*start_xy)
 
 # Todo: 4. Create 100 dots.
-tt.colormode(255)
-dots_count = range(1,101)
-for dot in dots_count:
-    color = rm.choice(rgb_colors)
+elevation = 50
+all_dots = range(1, 101)
+for dot in all_dots:
+    color = RM.choice(rgb_colors)
     turtle.dot(20, color)
     turtle.penup()
     turtle.fd(50)
     turtle.pendown()
 
-    if dot % 10 == 0 and dot != dots_count[-1]:
-        elevation = list(turtle.pos())[1]
-        elevation += 50
-        turtle.teleport(start_x, elevation)
-    elif dot == dots_count[-1]:
+    if dot == all_dots[-1]:
         turtle.hideturtle()
+    elif dot % 10 == 0:
+        turtle.teleport(start_xy[0], turtle.pos()[1] + elevation)
 
 # Todo: 5. Have screen remain open.
-# turtle_info()
-tt.exitonclick()
+screen.exitonclick()
