7d6d94e3e4a453f8713b54d34e958d82e3948134
rowbypierre <rowbypierre1998@gmail.com>
2025-11-07 16:47:46 -0500

    Add functions: save_score, high_score

--- day020-21-24_nokia_snake_game_oop/scoreboard.py
+++ day020-21-24_nokia_snake_game_oop/scoreboard.py
@@ -4,6 +4,7 @@ ALIGN = "center"
 FONT = ("Courier", 20, "normal")
 COLOR = "white"
 LOCATION = (0, 260)
+DATAFILE = "./data.txt"
 
 
 class Scoreboard(Turtle):
@@ -17,6 +18,7 @@ class Scoreboard(Turtle):
         self.goto(*LOCATION)
         self.color(COLOR)
         self.score = -1
+        self.file_score = self.high_score()
         self.score_refresh()
 
     def score_refresh(self):
@@ -25,13 +27,26 @@ class Scoreboard(Turtle):
         """
         self.clear()
         self.score += 1
-        prompt = f"Score: {self.score}"
+        prompt = f"Score: {self.score}  High Score: {self.high_score()}"
         self.write(arg=prompt, align=ALIGN, font=FONT)
 
     def game_over(self):
         """
-        Write "GAME OVER" at middle of screen.
+        Write "GAME OVER" at middle of screen and save score.
         """
         self.goto(x=0, y=0)
         self.pendown()
         self.write(arg="GAME OVER", align=ALIGN, font=FONT)
+        self.save_score()
+
+    def high_score(self):
+        "Return the greater of two scores (int: local highscore vs current score)."
+        with open(DATAFILE, "r") as data:
+            saved_score = int(data.read().strip())
+
+        return max([saved_score, self.score])
+
+    def save_score(self):
+        "Save gameplay score to local data.txt if greater than score within file."
+        with open(DATAFILE, "r+") as data:
+            data.write(str(self.high_score()))
