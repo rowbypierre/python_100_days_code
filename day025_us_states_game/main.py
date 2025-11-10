50d191a24c66d0ba9c9eaf7e9062cb29281129c2
rowbypierre <rowbypierre1998@gmail.com>
2025-11-10 04:49:20 -0500

    Allow exit from prompt, add export of  missing values as study.csv

--- day025_us_states_game/main.py
+++ day025_us_states_game/main.py
@@ -11,25 +11,33 @@ turtle.shape(map_image)
 
 datafile = "50_states.csv"
 data = pandas.read_csv(datafile)
-print(data)
 
-all_guess = []
-game_on = len(all_guess) < 50
-while game_on:
-    guess = screen.textinput(f"{len(all_guess)}/50 States Correct", "Enter another state:").lower().strip().title()
+states_guessed = []
+is_guessing = len(states_guessed) < 50
+while is_guessing:
+    state_guess = (
+        screen.textinput(
+            f"{len(states_guessed)}/50 States Correct", "Enter another state:"
+        )
+        .lower()
+        .strip()
+        .title()
+    )
 
-    if guess in data.state.values and guess not in all_guess:
-        all_guess.append(guess)
-        record = data.loc[data.state == guess].iloc[0]
-        point = (int(record.x), int(record.y))
+    if state_guess == "Exit":
+        is_guessing = False
+
+        states_missed = data[~data.state.isin(states_guessed)].state
+        states_missed.to_csv("./study.csv", index=False)
+    elif state_guess in data.state.values and state_guess not in states_guessed:
+        states_guessed.append(state_guess)
+        record = data.loc[data.state == state_guess].iloc[0]
+        map_point = (int(record.x), int(record.y))
 
         label_turtle = turtle.Turtle(shape="circle")
         label_turtle.speed("slowest")
         label_turtle.shapesize(0.3, 0.3)
         label_turtle.penup()
-        label_turtle.goto(*point)
-        label_turtle.write(guess, font=("Arial", 8, "normal"))
+        label_turtle.goto(*map_point)
+        label_turtle.write(state_guess, font=("Arial", 8, "normal"))
         label_turtle.hideturtle()
-
-
-screen.mainloop()
